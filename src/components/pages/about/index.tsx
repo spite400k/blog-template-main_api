30a411184ad5837721f3b26d861f386f8fe5a0c7
spite400k <spite400k@gmail.com>
2023-06-03 10:31:53 +0900

    commit

--- src/components/pages/about/index.tsx
+++
@@ -1,6 +0,0 @@
-import { MainLayout } from '@/components/features/app/Layout';
-import { Profile } from '@/components/features/app/Profile';
-
-export const About = () => (
-  <MainLayout className="lg:block" main={<Profile />} />
-);
